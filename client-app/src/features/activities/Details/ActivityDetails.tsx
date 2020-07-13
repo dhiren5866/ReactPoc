5ddb300c8c30491c7ff6eebf5d6f9b32bf07c259
Dhiren Pandya <dpandya@evoketechnologies.com>
2020-07-13 20:13:04 +0530

    React Routing

--- client-app/src/features/activities/Details/ActivityDetails.tsx
+++ client-app/src/features/activities/Details/ActivityDetails.tsx
@@ -1,13 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Card, Image, Button } from 'semantic-ui-react';
 import ActivityStore from '../../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
+import { RouteComponentProps, Link } from 'react-router-dom';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 
+interface DetailsParam {
+  id: string
+}
 
-const ActivityDetails: React.FC = () => {
+
+const ActivityDetails: React.FC<RouteComponentProps<DetailsParam>> = ({match,history}) => {
   const activityStore = useContext(ActivityStore);
-  const {selectedActivity:activity,openEditForm,cancelSelectedActivity} = activityStore;
+  const {activity,loadActivity,loadingInitial} = activityStore;
+
+  useEffect(() => {
+    loadActivity(match.params.id)
+  },[loadActivity,match.params.id]);
+
+  if(loadingInitial || !activity) return <LoadingComponent content='Loading activity...'></LoadingComponent>
+
   return (
     <Card fluid>
       <Image src={`/assets/categoryImages/${activity!.category}.jpg`} wrapped ui={false} />
@@ -22,8 +35,8 @@ const ActivityDetails: React.FC = () => {
       </Card.Content>
       <Card.Content extra>
         <Button.Group widths={2}>
-            <Button onClick={() => openEditForm(activity!.id)} basic color='blue' content='Edit' />
-            <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel' />
+            <Button as ={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit' />
+            <Button onClick={() => history.push('/activities')} basic color='grey' content='Cancel' />
         </Button.Group>
       </Card.Content>
     </Card>
